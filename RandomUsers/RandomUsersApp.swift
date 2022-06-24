2f1472f5b9e9f516d0c437264e2d76cac0bd7688
Omar Nava <omar.mac.97@gmail.com>
2022-06-24 12:50:10 -0500

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Ricardo Omar Martinez Nava on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
